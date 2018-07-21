36c58f856805c3d877f3f9f1b5cd0e573cee3a3d
MichaelVL <mvl.gh@network42.dk>
2018-07-21 17:34:33 +0200

    Remove project management, make ids zero-based

--- tests/smoketest.sh
+++ tests/smoketest.sh
@@ -3,7 +3,6 @@
 set -e
 set -x
 
-project=smoketest
 external_net=external_net
 image=cirros
 
@@ -16,17 +15,6 @@ function smoketest() {
 	echo "Requires image $image"
 	exit 1
     fi
-
-    echo "Creating project $project"
-
-    openstack project create --enable $project --description "No smoking allowed"
-    openstack role add --user admin --project $project _member_
-    openstack role add --user admin --project $project admin
-
-    openstack project list
-
-    old_project=$OS_PROJECT_NAME
-    export OS_PROJECT_NAME=$project
 
     echo "Creating network"
 
@@ -52,29 +40,22 @@ function smoketest() {
 
     echo "Creating instances"
 
+    openstack server create --flavor m1.tiny --image $image --nic net-id=$NETWORK_ID --security-group ssh-access smoke_server0
     openstack server create --flavor m1.tiny --image $image --nic net-id=$NETWORK_ID --security-group ssh-access smoke_server1
-    openstack server create --flavor m1.tiny --image $image --nic net-id=$NETWORK_ID --security-group ssh-access smoke_server2
 
+    ip0=$(openstack floating ip create $external_net | grep ' floating_ip_address ' | awk '{print $4}')
     ip1=$(openstack floating ip create $external_net | grep ' floating_ip_address ' | awk '{print $4}')
-    ip2=$(openstack floating ip create $external_net | grep ' floating_ip_address ' | awk '{print $4}')
 
+    openstack server add floating ip smoke_server0 $ip0
     openstack server add floating ip smoke_server1 $ip1
-    openstack server add floating ip smoke_server2 $ip2
-    
-    OS_PROJECT_NAME=$old_project
-
-    echo "Created smoketest instances in project $project"
 }
 
 
 function delete_all() {
-    old_project=$OS_PROJECT_NAME
-    export OS_PROJECT_NAME=$project
+    echo "Deleting resources"
 
-    echo "Deleting project $project"
-
+    openstack server delete smoke_server0
     openstack server delete smoke_server1
-    openstack server delete smoke_server2
 
     openstack router remove subnet smoke_router smoke_subnet
     openstack router delete smoke_router
@@ -83,19 +64,11 @@ function delete_all() {
 
     openstack security group delete ssh-access
     
-    openstack project purge --project $project
-
-    OS_PROJECT_NAME=$old_project
-
-    openstack project delete $project
-
-    echo "Deleted smoketest instances from project $project"
+    echo "Deleted smoketest instances"
 }
 
 
 function e2e_test() {
-    old_project=$OS_PROJECT_NAME
-    export OS_PROJECT_NAME=$project
 
     set +e
     
@@ -117,31 +90,31 @@ function e2e_test() {
 	exit 1
     fi
 
-    echo "Running e2e test in project $project"
+    echo "Running e2e test"
 
+    server0=$(openstack server list | grep smoke_server0)
+    ip0=$(echo $server0 | awk '{print $9}')
+    iip0=$(echo $server0 | awk '{gsub("smoke_net=","");gsub(",","");print $8}')
+    echo "Server0 ip=$ip0, internal ip=$iip0"
     server1=$(openstack server list | grep smoke_server1)
     ip1=$(echo $server1 | awk '{print $9}')
     iip1=$(echo $server1 | awk '{gsub("smoke_net=","");gsub(",","");print $8}')
     echo "Server1 ip=$ip1, internal ip=$iip1"
-    server2=$(openstack server list | grep smoke_server2)
-    ip2=$(echo $server2 | awk '{print $9}')
-    iip2=$(echo $server2 | awk '{gsub("smoke_net=","");gsub(",","");print $8}')
-    echo "Server2 ip=$ip2, internal ip=$iip2"
 
     echo "Testing connectivity using floating IPs"
-    sshpass -p gocubsgo ssh -q -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null cirros@$ip1 nc -z -w2 $ip2 22
+    sshpass -p gocubsgo ssh -q -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null cirros@$ip0 nc -z -w2 $ip1 22
     if [ $? -ne 0 ]; then
 	echo "*** No connectivity found"
 	exit 1
     fi
-    sshpass -p gocubsgo ssh -q -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null cirros@$ip1 nc -z -w2 $ip2 23
+    sshpass -p gocubsgo ssh -q -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null cirros@$ip0 nc -z -w2 $ip1 23
     if [ $? -eq 0 ]; then
 	echo "*** Unexpected connectivity found"
 	exit 1
     fi
 
     echo "Testing connectivity using private IPs"
-    sshpass -p gocubsgo ssh -q -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null cirros@$ip1 nc -z -w2 $iip2 22
+    sshpass -p gocubsgo ssh -q -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null cirros@$ip0 nc -z -w2 $iip1 22
     if [ $? -ne 0 ]; then
 	echo "*** No connectivity found"
 	exit 1
@@ -149,8 +122,6 @@ function e2e_test() {
 
 
     echo "End2end test OK"
-    
-    OS_PROJECT_NAME=$old_project
 }
 
 
